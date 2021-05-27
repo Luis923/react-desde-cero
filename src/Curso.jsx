61246be88fdca2f5422373eebe9e73a0c8ca467b
Daniel981 <2016200091@untels.edu.pe>
2021-05-27 10:21:55 -0500

    agregando expresiones jsx

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+const mayorDeEdad = edad => edad > 18
+
+const persona = {
+    "nombre":"Alberto",
+    "apellido":"Quiroga",
+    "edad":"29"
+}
+
 const Curso = () => (
     <article class="card">
             <div class="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
@@ -7,7 +15,8 @@ const Curso = () => (
             </div>
             <div class="card__data s-border s-radius-br s-radius-bl s-pxy-2">
                 <h3 class="t5 s-mb-2 s-center">
-                    Programación orientada a objetos con Go
+                    {/* puede devolver jscript o html */}
+                    {mayorDeEdad(persona.edad) ? "Soy mayor de edad" : <p>Soy menor</p>}
                 </h3>
                 <div class="s-mb-2 s-main-center">
                     <div class="card__teacher s-cross-center">
@@ -16,7 +25,7 @@ const Curso = () => (
                                 <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/d5060642-11c9-49cb-9029-fc0a3bf8ff94.png" alt="" />
                             </div>
                         </div>
-                        <span class="small">Alexys Lozada</span>
+                        <span class="small">{`${persona.nombre} ${persona.apellido}`} </span>
                     </div>
                 </div>
                 <div class="s-main-center">
