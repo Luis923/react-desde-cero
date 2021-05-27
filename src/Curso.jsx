488558e41e6db6af7d4f88c48beecb54dcce2a9c
Daniel981 <2016200091@untels.edu.pe>
2021-05-27 10:46:21 -0500

    Agregando propiedades

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 const mayorDeEdad = edad => edad > 18
 
+const curso={
+    "title": "React desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/big/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png",
+    "price": "50usd",
+    "profesor": "https://edteam-media.s3.amazonaws.com/users/thumbnail/d5060642-11c9-49cb-9029-fc0a3bf8ff94.png"
+}
+
 const persona = {
     "nombre":"Alberto",
     "apellido":"Quiroga",
@@ -11,7 +18,7 @@ const persona = {
 const Curso = () => (
     <article class="card">
             <div class="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-                <img src="https://edteam-media.s3.amazonaws.com/courses/big/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png" alt="" />
+                <img src={curso.image} alt={curso.title} />
             </div>
             <div class="card__data s-border s-radius-br s-radius-bl s-pxy-2">
                 <h3 class="t5 s-mb-2 s-center">
@@ -22,14 +29,14 @@ const Curso = () => (
                     <div class="card__teacher s-cross-center">
                         <div class="card__avatar s-mr-1">
                             <div class="circle img-container">
-                                <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/d5060642-11c9-49cb-9029-fc0a3bf8ff94.png" alt="" />
+                                <img src={curso.profesor} alt="" />
                             </div>
                         </div>
                         <span class="small">{`${persona.nombre} ${persona.apellido}`} </span>
                     </div>
                 </div>
                 <div class="s-main-center">
-                    <a class="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+                    <a class="button--ghost-alert button--tiny" href="#">{`${curso.price}`}</a>
                 </div>
             </div>
         </article>   
