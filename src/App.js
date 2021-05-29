ac8906a7625512304bffc0cc0a67b7110fc9c1ff
Luis923 <luis_gaspar923@hotmail.com>
2021-05-28 21:40:25 -0500

    Agregando Recorrido de arrays

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import React, { Fragment } from 'react';
 import "./styles/styles.scss";
 import Curso from './Curso'
 
+const cursos= [
+    {
+        "title": "React desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/big/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+        "price": 40,
+        "profesor": "https://edteam-media.s3.amazonaws.com/users/thumbnail/c3e463c6-8c4f-415e-b4c4-a2e283d9a205.png"
+    },
+    {
+        "title": "Dart desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/big/9e541efb-1c68-4252-ba2c-f57691a743a1.png",
+        "price": 20,
+        "profesor": "https://edteam-media.s3.amazonaws.com/users/thumbnail/8e0b61dd-6fb6-43e5-9028-525d9f060b64.jfif"
+    },
+    {
+        "title": "GO desde cero",
+        "image": "https://edteam-media.s3.amazonaws.com/courses/big/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png",
+        "price": 30,
+        "profesor": "https://edteam-media.s3.amazonaws.com/users/thumbnail/d5060642-11c9-49cb-9029-fc0a3bf8ff94.png"
+    }
+]
+
 /* creamos una variable de tipo funcion, retorna un elemento html */
 const App = () => (
     <Fragment>
@@ -19,18 +40,9 @@ const App = () => (
             </div>
         </div>
         <div className="ed-grid m-grid-3">
-            <Curso
-                title= "React desde cero"
-                /* image= "https://edteam-media.s3.amazonaws.com/courses/big/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png" */
-                price= "50usd"
-                profesor= "https://edteam-media.s3.amazonaws.com/users/thumbnail/d5060642-11c9-49cb-9029-fc0a3bf8ff94.png"
-            ></Curso>
-            <Curso
-                title= "Dart desde cero"
-                image= "https://edteam-media.s3.amazonaws.com/courses/big/9e541efb-1c68-4252-ba2c-f57691a743a1.png"
-                price= "30usd"
-                profesor= "https://edteam-media.s3.amazonaws.com/users/thumbnail/8e0b61dd-6fb6-43e5-9028-525d9f060b64.jfif"
-            ></Curso>
+            {
+                cursos.map( c => <Curso title={c.title} image={c.image} price={c.price} profesor={c.profesor}/>)
+            }
         </div>
 
     </Fragment>
